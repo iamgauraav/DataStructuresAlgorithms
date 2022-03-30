41a3e5f483c40f29711fb6fe81da249f8e56c881
iamgauraav <iamgauraav@gmail.com>
2022-03-30 23:25:11 +0530

    Added Welcome message to master branch

---
+++ DSAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structures and Algorithms on master branch");
+        }
+    }
+}
