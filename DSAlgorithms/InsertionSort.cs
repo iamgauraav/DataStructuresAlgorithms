58a6681b0895dc652c88e4ff436beaf9257585d1
iamgauraav <iamgauraav@gmail.com>
2022-04-15 21:23:08 +0530

    Added Insertion Sort

---
+++ DSAlgorithms/InsertionSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAlgorithms
+{
+    public class InsertionSort
+    {
+        public static void InsertionSortList(string[] list)
+        {
+            for (int i = 1; i < list.Count(); i++)
+            {
+                string word = list[i];
+                int j = i - 1;
+
+                while (j >= 0 && list[j].CompareTo(word) > 0)
+                {
+                    list[j + 1] = list[j];
+                    j = j - 1;
+                }
+                list[j + 1] = word;
+            }
+            Console.WriteLine("The sorted array is: ");
+
+        }
+        
+        static void Main(string[] args)
+        {
+            string[] list = { "beans", "apple", "orange", "pineapple", "mango" };
+            InsertionSortList(list);
+            foreach (var element in list)
+            {
+                Console.WriteLine(element + "\n");
+            }
+        }
+    }
+}
