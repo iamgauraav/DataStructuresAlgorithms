f844c892990cd9231f1211646ebcb5c1a4e6d56a
iamgauraav <iamgauraav@gmail.com>
2022-04-15 19:52:18 +0530

    Added All permutations of a String using iterative method

---
+++ DSAlgorithms/PermutationOfString.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DSAlgorithms
+{
+    class Program
+    {
+        public static void permute(string str, int l, int r)
+        {
+            if (l == r)
+            {
+                Console.WriteLine(str);
+            }
+            else
+            {
+                //from l --> r
+                for (int i = l; i <= r; i++)
+                {
+                    str = swap(str, l, i);
+                    permute(str, l + 1, r); //Find combination
+                    str = swap(str, l, i); //backtrack
+                }
+            }
+        }
+        public static string swap(string a, int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            string str = "ABC";
+            int n = str.Length;
+            permute(str, 0, n - 1);
+        }
+    }
+}
