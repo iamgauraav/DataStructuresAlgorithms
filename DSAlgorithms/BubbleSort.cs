a33e349c2f83fc83b6baaf6aa393e1e9175c1975
iamgauraav <iamgauraav@gmail.com>
2022-04-16 19:55:11 +0530

    Added Bubble sort method

---
+++ DSAlgorithms/BubbleSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DSAlgorithms
+{
+    class BubbleSort
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 78, 55, 45, 98, 13, 1 };
+            int temp;
+
+            for (int i = 0;  i< arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted:");
+            foreach (int p in arr)
+                Console.Write(p + " ");
+            Console.Read();
+        }
+
+
+    }
+    
+}
