178e15525d575c655dc39ba89079e1286b4929f6
iamgauraav <iamgauraav@gmail.com>
2022-04-17 15:45:03 +0530

    To find Prime number between 0 to 1000

---
+++ DSAlgorithms/PrimeNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace DSAlgorithms
+{
+    class PrimeNumber
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int number = 1; number < 1000; number++)
+            {
+                int count = 0;
+
+                for (int i = 1; i <= number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            
+        }
+    }
+}
